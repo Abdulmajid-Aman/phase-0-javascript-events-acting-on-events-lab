ae90b5758c2ffb465d372022254421f8cec4a6c2
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-07-05 17:15:21 +0300

    event acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const dodger = document.getElementById("dodger")
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+
+    if(left > 0) {
+        dodger.style.left = `${left - 10}px`
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+
+
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.left.replace("px", "") 
+    let right = parseInt(rightNumbers , 10)
+
+    if (right < 360) {
+        dodger.style.left = `${right + 10}px`
+    }
+}
+
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") {
+       moveDodgerRight()
+    }
+})
